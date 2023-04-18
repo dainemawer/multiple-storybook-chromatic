4a2ac439044cc716091de0892a274ceb04399bb3
Daine Mawer <hello@dainemawer.com>
2023-04-18 10:06:09 +0200

    initial setup

---
+++ projects/site-one/pages/index.tsx
@@ -0,0 +1,5 @@
+function HomePage() {
+    return <div>Welcome to Site One</div>
+}
+
+export default HomePage
---
+++ projects/site-two/pages/index.tsx
@@ -0,0 +1,5 @@
+function HomePage() {
+    return <div>Welcome to Site Two</div>
+}
+
+export default HomePage
